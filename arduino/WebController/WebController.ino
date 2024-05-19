a7dc005111f386ad8cb67f10f486f6168fd37ee2
Reyes546 <cannonreyes55@gmail.com>
2024-05-18 18:26:26 -0700

    refactored routes

--- arduino/WebController/WebController.ino
+++ arduino/WebController/WebController.ino
@@ -9,11 +9,37 @@
 WebServer server(8200);
 
 const int led = 13;
+int mode = 0;    // Currently-active animation mode, 0-9
+int RGBB[4] = {10, 0, 0, 10}; // R, G, B, (Brightness/100)%
 
 void handleRoot() {
   digitalWrite(led, 1);
   server.send(200, "text/plain", "hello from esp32!");
   digitalWrite(led, 0);
+}
+
+void registerArduino() {
+  digitalWrite(led, 1);
+  String message = "{ name : ";
+  message += NAME;
+  message += "; rgb: ";
+  message += "";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+String convertToHex() {
+  String hexValue = "";
+
+
+  return "";
 }
 
 void handleNotFound() {
@@ -32,16 +58,54 @@ void handleNotFound() {
   }
   server.send(404, "text/plain", message);
   digitalWrite(led, 0);
+
 }
 
-void registerArduino() {
+void test() {
+    digitalWrite(led, 1);
+  char temp[400];
+  int sec = millis() / 1000;
+  int min = sec / 60;
+  int hr = min / 60;
 
+  snprintf(temp, 400,
+
+           "<html>\
+  <head>\
+    <meta http-equiv='refresh' content='5'/>\
+    <title>ESP32 Demo</title>\
+    <style>\
+      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+    </style>\
+  </head>\
+  <body>\
+    <h1>Hello from ESP32!</h1>\
+    <p>Uptime: %02d:%02d:%02d</p>\
+    <img src=\"/test.svg\" />\
+  </body>\
+</html>",
+
+           hr, min % 60, sec % 60
+          );
+  server.send(200, "text/html", temp);
+  digitalWrite(led, 0);
+}
+
+void SetupRoutes() {
+  server.on("/", handleRoot);
+  server.on("/register", registerArduino);
+  server.on("/test", test);
+  server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
+  server.onNotFound(handleNotFound);
 }
 
 void setup(void) {
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
-  Serial.begin(115200);
+  Serial.begin(9600);
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PASSWORD);
   Serial.println("");
@@ -61,15 +125,7 @@ void setup(void) {
     Serial.println("MDNS responder started");
   }
 
-  server.on("/", handleRoot);
-
-  server.on("/register", registerArduino);
-
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
-
-  server.onNotFound(handleNotFound);
+  SetupRoutes();
 
   server.begin();
   Serial.println("HTTP server started");
